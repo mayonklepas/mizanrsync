704651413946ba30f811e46022b75e8f1c17f643
mayonklepas <imonks21@gmail.com>
2023-12-22 19:06:17 +0700

    Add files via upload

---
+++ rsyncjava/src/MizanRsync/Main.java
@@ -0,0 +1,169 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MizanRsync;
+
+import java.io.BufferedReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author mulyadi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileReader("config.properties"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String host = prop.getProperty("rsync.host");
+        String apiPort = prop.getProperty("rsync.apiPort");
+        String apiCompanyCode = prop.getProperty("rsync.apiCompanyCode");
+        String username = prop.getProperty("rsync.username");
+        String password = prop.getProperty("rsync.password");
+        String serverPath = prop.getProperty("rsync.serverpath");
+        String localPath = prop.getProperty("rsync.localpath");
+        String intervalString = prop.getProperty("rsync.interval");
+        int interval = Integer.parseInt(intervalString);
+        String preCommand = prop.getProperty("rsync.preCommand");
+
+        String os = System.getProperty("os.name");
+
+        ExecutorService serv = Executors.newSingleThreadExecutor();
+        serv.execute(() -> {
+            while (true) {
+                try {
+
+                    if (!preCommand.equals("none")) {
+                        Runtime rt = Runtime.getRuntime();
+                        Process p = rt.exec(preCommand);
+
+                        try ( BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+                            String result = br.lines().collect(Collectors.joining(System.lineSeparator()));
+                            System.out.println(result);
+                        }
+
+                        try ( BufferedReader brError = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+                            String resultError = brError.lines().collect(Collectors.joining(System.lineSeparator()));
+                            System.out.println(resultError);
+                        }
+                    }
+
+                    boolean isReadySync = new Main().getIsActiveSync(apiPort, apiCompanyCode);
+
+                    if (isReadySync) {
+
+                        Logger.getLogger(Main.class.getName()).info("Ready to sync");
+
+                        String command = "sshpass -p '{{password}}' rsync -avzP {{localPath}} {{username}}@{{host}}:{{serverPath}}";
+
+                        if (os.toLowerCase().contains("windows")) {
+                            command = "cmd.exe /c wsl sshpass -p '{{password}}' rsync -avzP {{localPath}} {{username}}@{{host}}:{{serverPath}}";
+                        }
+
+                        command = command.replace("{{password}}", password)
+                                .replace("{{localPath}}", localPath)
+                                .replace("{{username}}", username)
+                                .replace("{{host}}", host)
+                                .replace("{{serverPath}}", serverPath);
+
+                        String strip = Stream.iterate(0, x -> x + 1).limit(command.length()).map(d -> "-").collect(Collectors.joining());
+
+                        System.out.println("\n");
+                        System.out.println(strip);
+                        System.out.println(command);
+                        System.out.println(strip);
+
+                        Runtime rt = Runtime.getRuntime();
+                        Process p = rt.exec(command);
+
+                        try ( BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+                            String result = br.lines().collect(Collectors.joining(System.lineSeparator()));
+                            System.out.println(result);
+                        }
+
+                        try ( BufferedReader brError = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+                            String resultError = brError.lines().collect(Collectors.joining(System.lineSeparator()));
+                            System.out.println(resultError);
+                        }
+                    } else {
+                        Logger.getLogger(Main.class.getName()).info("Sync Not Active");
+                    }
+
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                try {
+                    Thread.sleep(interval);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        });
+        serv.shutdown();
+
+    }
+
+    public boolean getIsActiveSync(String port, String companyCode) {
+        try {
+            URL url = new URL("http://mizancloud.com:" + port + "/biling/user-layanan/check-active-backup-service?companyCode=" + companyCode);
+            Logger.getLogger(Main.class.getName()).info(url.toString());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            String result = new BufferedReader(new InputStreamReader(is)).lines().collect(Collectors.joining(System.lineSeparator()));
+            JSONParser parser = new JSONParser();
+            JSONObject obj = (JSONObject) parser.parse(result);
+            Map<String, Object> data = (Map) obj.get("data");
+            boolean isActiveCloud = (boolean) data.get("isActiveCloud");
+            boolean isActiveBackup = (boolean) data.get("isActiveBackup");
+            if (isActiveCloud == false && isActiveBackup == true) {
+                return true;
+            }
+            return false;
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ParseException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+}
